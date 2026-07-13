7187afc3c0864a38554bc1a0131ddafb2e25da07
Lauan097 <lauan.campos64@gmail.com>
2026-07-13 02:12:13 -0300

    feat: initialize bot application with project structure, Prisma configuration, and Discord client bootstrap

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,10 +1,11 @@
 import path from "node:path";
 import { defineConfig } from "prisma/config";
+import "dotenv/config";
 
 export default defineConfig({
     schema: path.join("prisma"),
     datasource: {
-        url: process.env.DATABASE_URL ?? "postgresql://system:j82XuW4D4HXERqk@marcos600:5432/pontobot-db"
+        url: process.env.DATABASE_URL
     },
     migrations: {
         seed: "tsx ./src/database/seed.ts"
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { bootstrap } from "@constatic/base";
 import "#server";
+import "dotenv/config";
 
 await bootstrap({
     meta: import.meta,
