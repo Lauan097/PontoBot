718ec1a23dd82699bce83ee753a03be1b1c163b0
Lauan097 <lauan.campos64@gmail.com>
2026-07-13 02:07:54 -0300

    feat: initialize project configuration with Prisma setup and package dependencies

--- apps/bot/prisma.config.ts
+++ apps/bot/prisma.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from "prisma/config";
 export default defineConfig({
     schema: path.join("prisma"),
     datasource: {
-        url: process.env.DATABASE_URL!
+        url: process.env.DATABASE_URL ?? "postgresql://system:j82XuW4D4HXERqk@marcos600:5432/pontobot-db"
     },
     migrations: {
         seed: "tsx ./src/database/seed.ts"
