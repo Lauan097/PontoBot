3332b24a5e7dfcec4c020b8353b34a6bc135d106
Lauan097 <lauan.campos64@gmail.com>
2026-07-13 01:03:56 -0300

    feat: initialize bot project structure and implement clientReady discord event handler

--- src/discord/events/clientReady.ts
+++ src/discord/events/clientReady.ts
@@ -2,7 +2,7 @@ import { createEvent } from "#base";
 
 createEvent({
     name: "Conected Client Discord",
-    event: "ready",
+    event: "clientReady",
     async run(client) {
         const sts = process.env.ENV === "dev" ? "idle" : "online";
         const txt =
