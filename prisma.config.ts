f15d596e61c90c70036729f040e676ebf41c6b2d
Lauan097 <lauan.campos64@gmail.com>
2026-07-12 22:58:23 -0300

    feat: add Prisma configuration file with environment variable loading and custom seed script

--- prisma.config.ts
+++ prisma.config.ts
@@ -2,16 +2,14 @@ import path from "node:path";
 import { loadEnvFile } from "node:process";
 import { defineConfig } from "prisma/config";
 
-loadEnvFile(
-    process.argv.includes("--dev") 
-        ? ".env.dev" // Usage: npx prisma@latest ... -- --dev
-        : ".env"
-);
+try {
+    loadEnvFile(".env");
+} catch {}
 
 export default defineConfig({
     schema: path.join("prisma"),
     datasource: {
-        url: process.env.DATABASE_URL! 
+        url: process.env.DATABASE_URL!
     },
     migrations: {
         seed: "tsx ./src/database/seed.ts"
